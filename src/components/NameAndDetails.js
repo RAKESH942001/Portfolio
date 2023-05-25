77d20d016e4ceee5088daeb37e9df82efafdb872
Rakesh <nlrakesh44@gmail.com>
2023-05-25 17:58:01 +0530

    Name And Details Components

--- src/components/NameAndDetails.js
+++ src/components/NameAndDetails.js
@@ -1,8 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 
 const NameAndDetails = () =>
-	<div>
-		<h1 className="name">I,m Rakesh NL</h1>
+	<div className="name">
+		<h1>Hi,I'm <div className="nameColor"> Rakesh NL</div>
+			<br/>Front-End Engineer
+		</h1>
 	</div>;
 
 export default NameAndDetails;
