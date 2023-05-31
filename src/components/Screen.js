53f413440a272118ee737473eea0a6876f337edd
Rakesh <nlrakesh44@gmail.com>
2023-05-31 19:13:41 +0530

    File path Changed and palced in folder.

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NameAndDetails from './NameAndDetails';
 import Header from './header/Header';
-import About from './header/tabs/About';
+import About from './header/tabs/about/About';
 const Screen = (context) =>
 	<div>
 		<Header { ...context }/>
