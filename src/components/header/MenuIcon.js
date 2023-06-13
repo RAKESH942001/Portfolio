ac695bb9a301526b3b656ad5293946cb89c56f2e
Rakesh <nlrakesh44@gmail.com>
2023-06-13 20:20:27 +0530

    New Icons Introduced Menu and Close Icon

---
+++ src/components/header/MenuIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const MenuIcons = () =>
+	<div className="menuIcon">
+		<MenuIcon/>
+	</div>;
+
+export default MenuIcons;
--- src/components/header/tabs/Tabs.js
+++ src/components/header/tabs/Tabs.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Tabs = () =>
-	<div className="tabs">
+	<div
+		className="tabs"
+	>
 		<div>	<a href="#">Home</a> </div>
 		<div><a href="#">About</a></div>
 		<div><a href="#">Services</a></div>
 		<div><a href="#">Portfolio</a></div>
 		<div><a href="#">Contact</a></div>
-	</div>;
+		<div className="closeIcon"><CloseIcon/></div>
+	</div>
+	;
 
 export default Tabs;
