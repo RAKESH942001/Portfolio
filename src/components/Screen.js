8cd20bb2bc007478040a6a04dcce32ba51e1552e
Rakesh <nlrakesh44@gmail.com>
2023-06-13 20:19:24 +0530

    created new Id

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -3,8 +3,10 @@ import NameAndDetails from './NameAndDetails';
 import About from './header/tabs/about/About';
 import Portfolio from './header/tabs/portFolio/Portfolio';
 import Contact from './header/tabs/contact/Contact';
+import Header from './header/Header';
 const Screen = (context) =>
-	<div>
+	<div id="backgroundImage" className="container">
+		<Header { ...context }/>
 		<NameAndDetails { ...context }/>
 		<About { ...context }/>
 		<Portfolio { ...context }/>
