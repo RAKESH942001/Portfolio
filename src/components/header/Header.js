8ac2dcc688e77e2680da6addd37a477ca3336bf8
Rakesh <nlrakesh44@gmail.com>
2023-06-13 20:19:51 +0530

    Refactored

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logo from './Logo';
 import Tabs from './tabs/Tabs';
+import MenuIcons from './MenuIcon';
 
 const Header = (context) =>
-	<div className="container">
+	<div className="header ">
 		<Logo { ...context }/>
 		<Tabs { ...context }/>
+		<MenuIcons { ...context }/>
 	</div>;
 
 export default Header;
